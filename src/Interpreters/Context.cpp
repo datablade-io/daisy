4318273ef3eced7ad376ad0b4efbd936cc533e35
Ken Chen <zlchen.ken@gmail.com>
2021-03-16 15:09:38 -0700

    virtual table (#9)

--- src/Interpreters/Context.cpp
+++ src/Interpreters/Context.cpp
@@ -2641,7 +2641,15 @@ void Context::setupNodeIdentity()
         return;
     }
 
-    node_identity = getFQDNOrHostName();
+    auto id = getConfigRef().getString("system_settings.node_identity", "");
+    if (!id.empty())
+    {
+        node_identity = id;
+    }
+    else
+    {
+        node_identity = getFQDNOrHostName();
+    }
 }
 
 void Context::setupQueryStatusPollId()
