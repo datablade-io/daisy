47fd20ba7aa76bd1770be98c051f25ff9a5aac6a
Ken Chen <zlchen.ken@gmail.com>
2021-03-23 22:33:08 +0000

    more robust schema validation

--- src/Server/RestRouterHandlers/SchemaValidator.cpp
+++ src/Server/RestRouterHandlers/SchemaValidator.cpp
@@ -11,32 +11,40 @@ namespace ErrorCodes
 
 void validateSchema(const std::map<String, std::map<String, String>> & schema, const Poco::JSON::Object::Ptr & payload)
 {
-    for (const auto & required : schema.at("required"))
+    auto iter = schema.find("required");
+    if (iter != schema.end())
     {
-        if (!payload->has(required.first))
+        for (const auto & required : iter->second)
         {
-            throw Exception("Required param '" + required.first + "' is missing.", ErrorCodes::POCO_EXCEPTION);
-        }
+            if (!payload->has(required.first))
+            {
+                throw Exception("Required param '" + required.first + "' is missing.", ErrorCodes::POCO_EXCEPTION);
+            }
 
-        if ((required.second == "int" && !payload->get(required.first).isInteger())
-            || (required.second == "string" && !payload->get(required.first).isString())
-            || (required.second == "bool" && !payload->get(required.first).isBoolean())
-            || (required.second == "double" && !payload->get(required.first).isNumeric())
-            || (required.second == "array" && !payload->get(required.first).isArray()))
-        {
-            throw Exception("Invalid type of param '" + required.first + "'", ErrorCodes::POCO_EXCEPTION);
+            if ((required.second == "int" && !payload->get(required.first).isInteger())
+                || (required.second == "string" && !payload->get(required.first).isString())
+                || (required.second == "bool" && !payload->get(required.first).isBoolean())
+                || (required.second == "double" && !payload->get(required.first).isNumeric())
+                || (required.second == "array" && !payload->get(required.first).isArray()))
+            {
+                throw Exception("Invalid type of param '" + required.first + "'", ErrorCodes::POCO_EXCEPTION);
+            }
         }
     }
 
-    for (const auto & optional : schema.at("optional"))
+    iter = schema.find("optional");
+    if (iter != schema.end())
     {
-        if (payload->has(optional.first)
-            && ((optional.second == "int" && !payload->get(optional.first).isInteger())
-                || (optional.second == "string" && !payload->get(optional.first).isString())
-                || (optional.second == "bool" && !payload->get(optional.first).isBoolean())
-                || (optional.second == "double" && !payload->get(optional.first).isNumeric())))
+        for (const auto & optional : iter->second)
         {
-            throw Exception("Invalid type of param '" + optional.first + "'", ErrorCodes::POCO_EXCEPTION);
+            if (payload->has(optional.first)
+                && ((optional.second == "int" && !payload->get(optional.first).isInteger())
+                    || (optional.second == "string" && !payload->get(optional.first).isString())
+                    || (optional.second == "bool" && !payload->get(optional.first).isBoolean())
+                    || (optional.second == "double" && !payload->get(optional.first).isNumeric())))
+            {
+                throw Exception("Invalid type of param '" + optional.first + "'", ErrorCodes::POCO_EXCEPTION);
+            }
         }
     }
 }
