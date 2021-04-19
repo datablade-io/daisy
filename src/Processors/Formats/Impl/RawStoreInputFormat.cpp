307cc229c8afede0288503f1bf34b1ab07589108
Tian Fan <sunset3000@sina.com>
2021-04-19 15:45:52 +0800

    Fix issue DPT-3987 some ISO-8601 timestamp cannot be recoganized in rawstore ingest api, DPT-3986 RawStore Ingest API does not support ingore 'enrichment' field

--- src/Processors/Formats/Impl/RawStoreInputFormat.cpp
+++ src/Processors/Formats/Impl/RawStoreInputFormat.cpp
@@ -52,7 +52,7 @@ RawStoreInputFormat::RawStoreInputFormat(
     const auto & time_extraction_type = format_settings_.rawstore.rawstore_time_extraction_type;
     const auto & time_extraction_rule = format_settings_.rawstore.rawstore_time_extraction_rule;
 
-    if (time_extraction_type != "json_path" && time_extraction_type != "regex")
+    if (!time_extraction_type.empty() && time_extraction_type != "json_path" && time_extraction_type != "regex")
         throw Exception(
             "time_extraction_type should be either 'json' or 'regex' " + time_extraction_type + " is not supported.",
             ErrorCodes::UNRECOGNIZED_ARGUMENTS);
@@ -255,7 +255,6 @@ void RawStoreInputFormat::readJSONObject(MutableColumns & columns)
         throw Exception("Logical error: no _raw column in the input data", ErrorCodes::LOGICAL_ERROR);
     }
 
-    seen_columns[time_col_idx] = read_columns[time_col_idx] = true;
     if (format_settings.rawstore.rawstore_time_extraction_type == "json_path")
         extractTimeFromRawByJSON(*columns[time_col_idx], *columns[raw_col_idx]);
     else if (format_settings.rawstore.rawstore_time_extraction_type == "regex" && time_extraction_regex != nullptr)
@@ -310,6 +309,7 @@ void RawStoreInputFormat::extractTimeFromRawByJSON(IColumn & time_col, IColumn &
 
     const auto & type = getPort().getHeader().getByPosition(time_col_idx).type;
     type->deserializeAsTextJSON(time_col, buf, format_settings);
+    seen_columns[time_col_idx] = read_columns[time_col_idx] = true;
 }
 
 void RawStoreInputFormat::extractTimeFromRawByRegex(IColumn & time_col, IColumn & raw_col)
@@ -338,6 +338,7 @@ void RawStoreInputFormat::extractTimeFromRawByRegex(IColumn & time_col, IColumn 
 
     const auto & type = getPort().getHeader().getByPosition(time_col_idx).type;
     type->deserializeAsTextJSON(time_col, buf, format_settings);
+    seen_columns[time_col_idx] = read_columns[time_col_idx] = true;
 }
 
 bool RawStoreInputFormat::readRow(MutableColumns & columns, RowReadExtension & ext)
