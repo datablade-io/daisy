f34aea9ff5e6b09d372a964187b7fb252babafc8
zhongbao.zhang <zhangzhongbao@360.cn>
2021-07-09 11:12:50 +0800

    fixed

--- src/Server/RestRouterHandlers/SpecDocHandler.cpp
+++ src/Server/RestRouterHandlers/SpecDocHandler.cpp
@@ -16,31 +16,34 @@ namespace
 {
 String getSpecPath(const String & config_path)
 {
-    String spec_path = "";
-    Poco::Path path(config_path);
+    Poco::Path config_path_(config_path);
+    Poco::Path spec_path_;
 
-    if (!path.parent().toString().empty() && Poco::File(path.parent().toString() + "/clickhouse-spec").exists())
+    if (config_path_.depth() > 1)
     {
-        spec_path = path.parent().toString() + "/clickhouse-spec/";
+        spec_path_ = config_path_.parent().parent();
+        spec_path_ = spec_path_.append("clickhouse-spec");
+        if (Poco::File(spec_path_).exists())
+            return spec_path_.toString();
     }
     else
     {
-        String home_path = getenv("HOME");
-        if (Poco::File(path.current() + "/spec/").exists())
+        spec_path_ = config_path_.current();
+        spec_path_ = spec_path_.append("spec");
+        if (Poco::File(spec_path_).exists())
+            return spec_path_.toString();
+
+        spec_path_ = getenv("HOME");
+        spec_path_ = spec_path_.append("clickhouse-spec");
+        if (Poco::File(spec_path_).exists())
+            return spec_path_.toString();
+
+        if (Poco::File("/etc/clickhouse-spec/").exists())
         {
-            spec_path = path.current() + "/spec/";
-        }
-        else if (!home_path.empty() && Poco::File(home_path + "/clickhouse-spec/").exists())
-        {
-            spec_path = home_path + "/clickhouse-spec/";
-        }
-        else if (Poco::File("/etc/clickhouse-spec/").exists())
-        {
-            spec_path = "/etc/clickhouse-spec/";
+            return "/etc/clickhouse-spec/";
         }
     }
-
-    return spec_path;
+    return "";
 }
 
 }
@@ -48,13 +51,15 @@ String getSpecPath(const String & config_path)
 std::pair<String, Int32> SpecDocHandler::executeGet(const Poco::JSON::Object::Ptr & /*payload*/) const
 {
     const String  & spec_path = getSpecPath(query_context->getConfigPath());
-    Poco::Path path(spec_path + "rest-api");
+    Poco::Path path(spec_path);
+    path.append("rest-api");
 
     if (!Poco::File(path).exists())
     {
         return {
             jsonErrorResponse(
-                "The clickhouse-spec file could not be found, please keep it consistent with the clickhouse-server path",
+                "The clickhouse-spec file : '" + path.toString()
+                    + "' could not be found, please keep it consistent with the clickhouse-server path",
                 ErrorCodes::RESOURCE_NOT_FOUND),
             HTTPResponse::HTTP_NOT_FOUND};
     }
